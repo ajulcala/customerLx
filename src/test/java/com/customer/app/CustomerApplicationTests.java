e9515f9adf0a40b9423f9359b3491b12e1f9ae5b
ajulcala <ajulcala@everis.com>
2021-11-17 18:00:19 -0500

    Pruebas Unitarias

--- src/test/java/com/customer/app/CustomerApplicationTests.java
+++ src/test/java/com/customer/app/CustomerApplicationTests.java
@@ -1,13 +1,50 @@
 package com.customer.app;
 
+import com.customer.app.models.documents.Customer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerApplicationTests {
+	@Autowired
+	private WebTestClient client;
+	@Test
+	public void listarTest() {
+		client.get()
+				.uri("/customer/list")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Customer.class)
+				.consumeWith(r -> {
+					List<Customer> customer = r.getResponseBody();
+					Assertions.assertTrue(!(customer.size() <= 0));
+				});
+	}
 
 	@Test
-	void contextLoads() {
+	public void seeDni() {
+		client.get()
+				.uri("/customer/documentNumber/{number}", Collections.singletonMap("number","12345678"))
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(Customer.class)
+				.consumeWith(r -> {
+					Customer customer = r.getResponseBody();
+					Assertions.assertTrue(!(customer.getNames().isEmpty()));
+					Assertions.assertTrue(!(customer.getAddress().isEmpty()));
+				});
 	}
 
 }
